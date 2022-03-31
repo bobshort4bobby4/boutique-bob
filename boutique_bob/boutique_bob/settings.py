5e75b175ed3211dc1772ff4e6c4a927e3987a1d6
bobshort4bobby4 <treborken@gmail.com>
2022-03-31 20:29:00 +0000

    ADD allauth and base templates

--- boutique_bob/boutique_bob/settings.py
+++ boutique_bob/boutique_bob/settings.py
@@ -73,15 +73,11 @@ TEMPLATES = [
 ]
 
 AUTHENTICATION_BACKENDS = [
-    
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
-
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
-    
 ]
-
 SITE_ID = 1
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
